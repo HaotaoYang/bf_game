09c7b2e056812d6b75b8beaea2c83ef94dddbe39
Unknown <520023290@qq.com>
2017-08-15 17:33:09 +0800

    fix mnesia start

--- lib/db/mnesia/mnesia_tab_supervisor.ex
+++ lib/db/mnesia/mnesia_tab_supervisor.ex
@@ -13,23 +13,26 @@ defmodule MnesiaTab.Supervisor do
 
   def init([]) do
     init_schema()
+    Application.ensure_started(:mnesia)
     children = [worker(MnesiaTab, [], restart: :permanent)]
     supervise children, strategy: :simple_one_for_one
   end
 
   defp init_schema() do
     dir_name = List.to_string(:mnesia.system_info(:directory)) <> "/"
+    :ok = Application.stop(:mnesia)
     case :filelib.ensure_dir(dir_name) do
       :ok ->
         {:ok, file_list} = :file.list_dir(dir_name)
         case file_list == [] do
           true ->
-            ret = :mnesia.create_schema([node()])
-            Logger.debug "init db schema suc :   #{inspect(ret)}"
+            case :mnesia.create_schema([node()]) do
+              :ok -> Logger.debug "init db schema successfully..."
+              _ -> Logger.error "init db schema error!!!"
+            end
           _ -> 
             :ok
         end
-        Logger.debug "init mnesia schema successfully..."
       {:error, reason} ->
         Logger.error "mnesia dir error: #{inspect(reason)}"
     end
