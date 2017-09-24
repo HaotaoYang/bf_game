c7026220525603f49586b2febf1e6ccea3cd819e
HaotaoYang <520023290@qq.com>
2017-09-24 11:33:41 +0800

    fix gen_tar.sh

--- gen_tar.sh
+++ gen_tar.sh
@@ -1,11 +1,12 @@
+echo "请输入发布环境:"
+read ENV
 echo "请输入版本号:"
 read VERSION
 
-MIX_ENV=prod mix release --env=prod 
+MIX_ENV=$ENV mix release --env=$ENV
 
-if [ ! -e "tars" ]; then
-	mkdir tars
+if [ ! -e "tars/$ENV" ]; then
+	mkdir -p tars/$ENV
 fi
 
-mv ./_build/prod/rel/bf_game/releases/$VERSION/bf_game.tar.gz ./tars/$VERSION.tar.gz
-
+mv ./_build/$ENV/rel/bf_game/releases/$VERSION/bf_game.tar.gz ./tars/$ENV/bf_game-$VERSION.tar.gz
