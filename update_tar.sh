a9d506661762b61f7a44259358daa9973dbe060d
HaotaoYang <520023290@qq.com>
2017-11-14 09:18:11 -0600

    update update_tar.sh
    同步完善升级脚本

--- update_tar.sh
+++ update_tar.sh
@@ -1,12 +1,15 @@
 echo "请输入发布环境:"
 read ENV
-echo "请输入升级后的版本号:"
-read VERSION
+
+APP=$(grep 'app:' mix.exs | cut -d: -f3 | cut -d, -f1)
+VERSION=$(grep 'version' mix.exs | cut -d\" -f2)
+echo $APP
+echo $VERSION
 
 MIX_ENV=$ENV mix release --env=$ENV --upgrade
 
 if [ ! -e "tars/$ENV" ]; then
-    mkdir -p tars/$ENV
+	mkdir -p tars/$ENV
 fi
 
-mv ./_build/$ENV/rel/bf_game/releases/$VERSION/bf_game.tar.gz ./tars/$ENV/bf_game-$VERSION.tar.gz
+cp ./_build/$ENV/rel/$APP/releases/$VERSION/$APP.tar.gz ./tars/$ENV/$APP-$VERSION.tar.gz
